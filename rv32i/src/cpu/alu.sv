84db6ac70ba0c4807520af2df2866a6a3939c8c6
24ashitani <goma.pyra@gmail.com>
2026-08-30 21:56:25 +0900

    alu

---
+++ rv32i/src/cpu/alu.sv
@@ -0,0 +1,16 @@
+typedef enum logic [4:0] {A = 1} op_type_e;
+
+module alu (
+    input  logic [31:0] a,
+    input  logic [31:0] b,
+    input  logic [ 4:0] op_type,
+    output logic [31:0] out
+);
+
+  always_comb begin
+    case (op_type)
+      A: out = a;
+    endcase
+  end
+
+endmodule
