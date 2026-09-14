7b2da2513ee1178c009a5424bc82144f62024a6e
24ashitani <goma.pyra@gmail.com>
2026-09-14 18:47:10 +0900

    fmt

--- ml_compiler/src/vars.rs
+++ ml_compiler/src/vars.rs
@@ -26,7 +26,17 @@ pub fn ir_func_code_to_vartree(code: IRCode, label: String) -> Result<VarTree, S
         let mut var_defined_new = Vec::new();
         let mut var_free_new = Vec::new();
         match c {
-            IRExpr::ADD(x, y, z) | IRExpr::SUB(x, y, z) | IRExpr::GEQ(x, y, z) | IRExpr::LEQ(x, y, z) | IRExpr::GT(x, y, z) | IRExpr::LT(x, y, z) | IRExpr::EQ(x, y, z) | IRExpr::NEQ(x, y, z) | IRExpr::AND(x, y, z) | IRExpr::OR(x, y, z) | IRExpr::APP(x, y, z) => {
+            IRExpr::ADD(x, y, z)
+            | IRExpr::SUB(x, y, z)
+            | IRExpr::GEQ(x, y, z)
+            | IRExpr::LEQ(x, y, z)
+            | IRExpr::GT(x, y, z)
+            | IRExpr::LT(x, y, z)
+            | IRExpr::EQ(x, y, z)
+            | IRExpr::NEQ(x, y, z)
+            | IRExpr::AND(x, y, z)
+            | IRExpr::OR(x, y, z)
+            | IRExpr::APP(x, y, z) => {
                 var_defined_new.push(x);
                 var_free_new.push(y);
                 var_free_new.push(z);
@@ -35,7 +45,10 @@ pub fn ir_func_code_to_vartree(code: IRCode, label: String) -> Result<VarTree, S
                 var_defined_new.push(x);
                 var_free_new.push(y);
             }
-            IRExpr::LOADINT(x, _) | IRExpr::LOADBOOL(x, _) | IRExpr::LOADSTR(x, _) | IRExpr::LOADUNIT(x) => {
+            IRExpr::LOADINT(x, _)
+            | IRExpr::LOADBOOL(x, _)
+            | IRExpr::LOADSTR(x, _)
+            | IRExpr::LOADUNIT(x) => {
                 var_defined_new.push(x);
             }
             IRExpr::BRANCH(x, _) | IRExpr::RETURN(x) => {
@@ -44,7 +57,11 @@ pub fn ir_func_code_to_vartree(code: IRCode, label: String) -> Result<VarTree, S
             IRExpr::LOADFUNC(label, f) => {
                 var_defined_new.push(label);
                 let subfunc_res = ir_func_code_to_vartree(code.clone(), f);
-                if let Ok(tree) = subfunc_res {children.push(tree)} else {return subfunc_res};
+                if let Ok(tree) = subfunc_res {
+                    children.push(tree)
+                } else {
+                    return subfunc_res;
+                };
             }
             _ => {}
         }
@@ -74,7 +91,6 @@ fn ir_lookup_fn(code: IRCode, label: String) -> Option<FuncCode> {
     return None;
 }
 
-
 fn write_to_map(map: &mut VarMap, func: &VarTree, scope: Vec<(String, i32, String)>) {
     // println!("map:{:?}\nfunc:{:?}\nscope:{:?}", map, func, scope);
     let mut map_local = LocalVarMapping::new();
@@ -87,13 +103,13 @@ fn write_to_map(map: &mut VarMap, func: &VarTree, scope: Vec<(String, i32, Strin
         for c in children.iter() {
             if let VarTree::VAR(s) = c {
                 map_local.push((s.to_string(), i));
-                scope_.push((s.to_string(),0,func_name.to_string()));
+                scope_.push((s.to_string(), 0, func_name.to_string()));
                 i += 1;
-            } else if let VarTree::FUNC(_,_,_) = c {
+            } else if let VarTree::FUNC(_, _, _) = c {
                 let mut scope_2 = scope_.clone();
                 for j in 0..scope_2.len() {
                     let (v, n, f) = &scope_2[j];
-                    scope_2[j] = (v.to_string(), n+1, f.to_string());
+                    scope_2[j] = (v.to_string(), n + 1, f.to_string());
                 }
                 write_to_map(map, &c, scope_2);
             }
